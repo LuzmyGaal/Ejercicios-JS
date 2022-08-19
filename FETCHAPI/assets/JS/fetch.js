87c50183cf3aa44cea905ec5cfa7681d90270fa2
LuzmyGaal <luzmyaylin.garciaa@gmail.com>
2022-08-19 14:38:34 -0500

    actualizacion

--- FETCHAPI/assets/JS/fetch.js
+++ FETCHAPI/assets/JS/fetch.js
@@ -167,24 +167,88 @@ fetch('https://pokeapi.co/api/v2/pokemon/pikachu') //realizamos la peticion a la
             info.name, "y su numero es: ", info.id);
     });
 
+/*     
 
-    //Solicitud GET 
-    fetch("https://fakestoreapi.com/products/10") //realizamos la peticion a una URL
+Cuando hablamos de la API fetch, tambien debemos de hablar de los endpoints, que son las direcciones de las peticiones que se realizan. 
+
+
+Por ejemplo, en la URL https://pokeapi.co/api/v2/pokemon/pikachu, nosotros nos conectamos a la pokeapi, buscamos dentro de la categoria pokemon, y traemos la informacion de pikachu. En este caso, pikachu es nuestro endpoint
+
+*/
+
+//function fetchPokemon(id){ //creamos una funcion que recibe un parametro llamado id
+//fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`) //la peticion a la url, donde usamos el parametro id para obtener la informacin del pokemon.
+//.then(respuesta => respuesta.json()) //almacenamos la respuesta en un formato .json
+//.then(dato => console.log(dato)) //imprimimos la respuesta en consola
+//}
+//fetchPokemon(420);
+
+
+
+//Solicitud GET 
+
+
+fetch("https://fakestoreapi.com/products/10") //realizamos la peticion a una URL
     .then((datos) => {
-      //cuando la promesa es resuelta, entonces ejecutamos esta funcion
-      return datos.json(); //convertimos la respuesta a un formato JSON
+        //cuando la promesa es resuelta, entonces ejecutamos esta funcion
+        return datos.json(); //convertimos la respuesta a un formato JSON
     })
     .then((info) => {
-      //cuando la promesa es resuelta, entonces ejecutamos esta funcion. Estamos usando el metodo .then para obtener la respuesta de la promesa y guardarla en la variable info. Gracias a esto podre obtener la informacion que queremos.
-      console.log(
-        "El nombre de nuestro producto es:",
-        info.title,
-        " y su precio es: ",
-        info.price
-      ); //imprimimos el nombre del producto. esto es posible por que la respuesta ya esta en formato JSON, y podemos acceder a los datos directamente, especificando el nombre del campo que queremos.
+        //cuando la promesa es resuelta, entonces ejecutamos esta funcion. Estamos usando el metodo .then para obtener la respuesta de la promesa y guardarla en la variable info. Gracias a esto podre obtener la informacion que queremos.
+        console.log(
+            "El nombre de nuestro producto es:",
+            info.title,
+            " y su precio es: ",
+            info.price
+        ); //imprimimos el nombre del producto. esto es posible por que la respuesta ya esta en formato JSON, y podemos acceder a los datos directamente, especificando el nombre del campo que queremos.
     });
 
-    /* APIS DE ALMACENAMIENTO WEB
+
+
+/* 
+
+Solicitud POST para enviar informacion
+
+fetch('https://fakestoreapi.com/products',{
+           method:"POST", //por default es GET, para cambiarlo debemos especificar el metodo
+          body:JSON.stringify(//como la informacion que voy a manejar es un objeto, necesito convertirlo a un texto (string)
+            
+//cuerpo de la solicitud    
+{
+            title: 'Chettos Naranjas',
+                     price: 3.0,
+                     description: 'Deliciosos Chettos Naranjas',
+                     image: 'https://i.pravatar.cc',
+                     category: 'jewelery'
+                 }}
+)
+})
+
+.then(res=>res.json())
+.then(json=>console.log(json))
+
+
+//Solicitud POST para enviar informacion
+     fetch('https://fakestoreapi.com/products',{
+         method:"POST", //por default es GET, para cambiarlo debemos especificar el metodo
+         body:JSON.stringify(//como la informacion que voy a manejar es un objeto, necesito convertirlo a un texto (string)
+        
+ //cuerpo de la solicitud    
+             {
+                 title: 'Cacahuates',
+                 price: 3.0,
+                 description: 'Deliciosos Cacahuates Saladitos',
+                 image: 'https://i.pravatar.cc',
+                category: 'jewelery'
+}
+)
+})
+.then(res=>res.json())
+.then(json=>console.log(json))
+
+*/
+
+/* APIS DE ALMACENAMIENTO WEB
 
 
 la API de almacenamiento web define dos mecanismos de almacenamiento que son muy importantes
@@ -200,7 +264,121 @@ Algunas caracteristicas que comparten el local y el session storage son:
     - La capacidad (5Mb a diferencia de los 4Kb de las cookies)
     - La informacion es almacenada en pares clave/valor, por lo que se puede usar como si fueran variables.
     - El almacenamiento web solo es accesible en el navegador, no se envia al servidor como lo hacen las cookies.
-    
+
+
+ALMACENAMIENTO LOCAL (LocalStorage)
+
+La informacion almacenada con localStorage se guarda indefinidamente hasta que sea eliminada mediante codigo o bien borrada desde el navegador.
+
+El almacenamiento local es similiar a las cookies, pero se borra con menos frecuencia y puede almacenar mas datos. Por lo tanto, se puede utilizar en situaciones similares, tales como: almacenar articulos que un usuario de comercio electronico agrego a un carrito, almacenar historial de uso, etc.
+
+La informacion que almacenamos en localStorege se elimina hasta que se haga explicitamente, ya sea por nostros o por el usuario. Nunca se limpia automaticamente y se comparte en todas las sesiones que acceder al sitio.
+
+
+ALMACENAMIENTO DE SESION (SessionStorage)
+
+
+La informacion que guardamos en sessionStorage solo se guarda durante la sesion del navegador (es decir, se elimina cuando se cierra el navegador). Si tenemos varias ventanas o pestañas abiertas, la informacion se guarda en cada una de ellas, asi que si cerramos una ventana, la informacion se borra de la misma.
+
+
+Como accedemos al almacenamiento?
+
+window.localStorage
+windows.sessionStorage
 
 */
 
+/*Guardar datos (localStorage.setItem(key,value))
+
+La sintaxis para guardar datos en un localStorage es: localStorage.setItem(key, value);, donde key es la clave (o el identificador) y value es el valor que queremos guardar.
+*/
+
+
+// localStorage.setItem("Nombre","Felipe");
+// localStorage.setItem("Apellido","Maqueda");
+// localStorage.setItem("Edad", 30);
+// localStorage.setItem("Es programador?", true);
+
+// localStorage.setItem("Nombre1","Gabriel");
+// localStorage.setItem("Apellido1","Campos");
+// localStorage.setItem("Edad1", 28);
+// localStorage.setItem("Es programador?1", false);
+
+// localStorage.setItem("Nombre2","Andrea");
+// localStorage.setItem("Apellido2","Perez");
+// localStorage.setItem("Edad2", 28);
+// localStorage.setItem("Es programador?2", true);
+
+
+
+/*
+Recuperar datos (local.Storage.getItem(key, value))
+
+La sintaxis para obtener un dato de localStorage es: localStorage.getItem(key);, donde key es la clave (o el identificador) que queremos recuperar.
+
+// */
+
+// let recuperarNombre = localStorage.getItem("Nombre");
+// console.log(recuperarNombre);
+
+// let recuperarApellido = localStorage.getItem("Apellido");
+// console.log(recuperarApellido);
+
+// let recuperarProgramador = localStorage.getItem("Es programador?1");
+// console.log(recuperarProgramador);
+
+
+// //Remover datos de LocalStorage (localStorage.removeItem(key))
+
+// localStorage.removeItem("Nombre"); //borrar a Felipe
+// localStorage.removeItem("Apellido"); //borrar a Maqueda
+
+
+// //Conocer el largo de nuestro almacenamiento (localStorage.length)
+
+// let longitudLocalStorage = localStorage.length;
+// console.log(longitudLocalStorage);
+
+// //Limpiar datos del localStorage (local.Storage.clear())
+// localStorage.clear();
+
+
+// //Agregar elementos con sessionStorage
+// sessionStorage.setItem("Perrito", "Rocky");
+// sessionStorage.setItem("Gatito", "Bigotes");
+// sessionStorage.setItem("Nutria", "Hernesto");
+// sessionStorage.setItem("Caracol", "Gary");
+// sessionStorage.setItem("Conejtio", "Griselo");
+
+// let longitudSessionStorage = sessionStorage.length;
+// console.log(longitudSessionStorage);
+
+
+function guardarDatos() {
+
+    localStorage.nombre = document.getElementById("nombre").value; //guardar el valor de nombre en localStorage
+
+    localStorage.password = document.getElementById("password").value; //guardar el valor de password en localStorage
+
+    document.getElementById("datos").innerHTML = ("Datos guardados correctamente"); //sobreescribimos el contenido del parrafo por este nuevo mensaje
+
+    //Revisar el funcionamiento correcto de mi codigo
+    console.log(typeof localStorage.nombre);
+    console.log(typeof localStorage.password);
+
+}
+
+
+function recuperarDatos() {
+
+
+    //Si en en la llave nombre de mi localStrogane y ademas en la llave password los datos son diferentes de undefined (significa que si tengo datos), voy a mostrar el mensaje con esa informacion
+    if (localStorage.nombre != undefined && localStorage.password != undefined) {
+        document.getElementById("datos").innerHTML =
+            "Nombre: " + localStorage.nombre + " Password: " + localStorage.password;
+        //en caso de que no se cumpla la condicion, muestro un mensaje de alerta
+    } else {
+        document.getElementById("datos").innerHTML =
+            "No has introducido tu nombre y tu password!!!";
+    }
+}
